a6bafc2078d77277c82773dd7827d2aa4057b1cc
tngan03 <128450228+tngan03@users.noreply.github.com>
2024-04-02 22:25:34 +0700

    47th commit

---
+++ DonHangDAL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn1_QuanLyPhanMemBanPKDT.DataAcsess
+{
+    public class DonHangDAL
+    {
+        QLPKDTEntities1 db=new QLPKDTEntities1();
+        public List<DonHang> GetDonHangs ()
+        {
+            return db.DonHang.ToList<DonHang>();
+        }
+        public bool KiemTraKhoaNgoai(string maDH)
+        {
+            bool KetQua = db.ChiTietDonHang.Any(o => o.MaDH == maDH);
+            return KetQua;
+        }
+        public DonHang GetThongTin1Donhang(string MaDH)
+        {
+            DonHang DHTG = db.DonHang.FirstOrDefault(s => s.MaDH == MaDH);
+            return DHTG;
+        }
+
+        public void AddDH(DonHang dh)
+        {
+            db.DonHang.Add(dh);
+            db.SaveChanges();
+        }
+        public void DeleteDH(DonHang dh)
+        {            
+            db.DonHang.Remove(dh);
+            db.SaveChanges();
+        }
+        
+        //Thao tác với Chitietdonhang
+        public void AddCTDH(ChiTietDonHang dh)
+        {
+            db.ChiTietDonHang.Add(dh);
+            db.SaveChanges();
+        }
+        public void DeleteCTDH(string Madh)
+        {
+            var ctdhList = db.ChiTietDonHang.Where(ctdh => ctdh.MaDH == Madh).ToList();
+            db.ChiTietDonHang.RemoveRange(ctdhList);
+            db.SaveChanges();
+        }
+        public List<ObjectSP> GetListSPmua(string MaDH)
+        {
+            var Ketqua = from dhct in db.ChiTietDonHang
+                         join sp in db.SanPham on dhct.MaSP equals sp.MaSP
+                         where dhct.MaDH == MaDH
+                         select new ObjectSP
+                         {
+                             MaSP = dhct.MaSP,
+                             TenSP = sp.TenSP,
+                             Soluong = (int)dhct.SoLuong,
+                             Giaban = (double)sp.Giaban,
+                             Thanhtien = (double)dhct.DonGia,
+
+                         };
+            return Ketqua.ToList();
+        }
+
+
+    }
+}
---
+++ KhoHangDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoAn1_QuanLyPhanMemBanPKDT.DataAcsess;
+
+namespace DoAn1_QuanLyPhanMemBanPKDT.DataAcsess
+{
+    public class KhoHangDAL
+    {
+        QLPKDTEntities1 db=new QLPKDTEntities1();
+        public List<KhoHang> GetDataKhoHang()
+        {
+            return db.KhoHang.ToList<KhoHang>();
+        }
+        public void AddSP(KhoHang sanpham)
+        {
+            db.KhoHang.Add(sanpham);
+            db.SaveChanges();
+        }
+        public void DeleteSP(KhoHang sanpham)
+        {
+            db.KhoHang.Remove(sanpham);
+            db.SaveChanges();
+            
+        }
+        public void UpdateSP(KhoHang sanphamnew)
+        {
+            KhoHang sanphamOld = db.KhoHang.FirstOrDefault(p => p.MaSP == sanphamnew.MaSP);
+            if(sanphamOld!=null)
+            {
+                sanphamOld.Soluong = sanphamnew.Soluong+sanphamOld.Soluong;
+                sanphamOld.NgayNhap = sanphamnew.NgayNhap;
+                db.SaveChanges();
+            }    
+        }  
+        public void GiamSoLuong(KhoHang sp,int x)
+        {          
+                sp.Soluong=sp.Soluong-x;
+                db.SaveChanges();
+                
+        }
+        public void TangSoLuong(KhoHang sp, int x)
+        {
+            sp.Soluong = sp.Soluong+x;
+            db.SaveChanges();
+
+        }
+    }
+}
